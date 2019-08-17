75357bc23e1169d489f982a2369691a44e7f1f38
Emerson Pinheiro <emersonline2007@gmail.com>
2019-08-17 12:32:24 -0400

    teste

---
+++ tests/spec/conf.js
@@ -0,0 +1,15 @@
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+
+exports.config = {
+    framework: 'jasmine',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['teste_protractor.js'],
+
+    onPrepare: function(){
+        jasmine.getEnv().addReporter(new Jasmine2HtmlReporter({
+            savePath: './tests/reports/'
+        }))
+    }
+  }
+
+  
